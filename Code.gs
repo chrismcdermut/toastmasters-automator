b7e33458f3ca29251e7b45f56edd32859c95c650
Chris McDermut <chrismcdermut@gmail.com>
2020-05-29 10:33:05 -0700

    got directories working

--- Code.gs
+++
@@ -1,9 +0,0 @@
-function doGet(request) {
-  return HtmlService.createTemplateFromFile('Page')
-      .evaluate();
-}
-
-function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename)
-      .getContent();
-}
---
+++ scripts/code.js
@@ -0,0 +1,23 @@
+/* eslint no-undef: 0 */ // --> OFF
+/* eslint no-unused-vars: 0 */ // --> OFF
+
+/**
+ * Create the menu item and open it.
+ * @return {string} returns the html content.
+ */
+function doGet() {
+  return HtmlService.createTemplateFromFile('Page')
+      .evaluate();
+}
+
+/**
+ * Create the menu item and open it.
+ * @param {string} filename The file to import.
+ * @return {string} returns the html content.
+ */
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+      .getContent();
+}
+
+// module.exports = {doGet, include};
